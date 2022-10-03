94303c0007561fb9084bf48f1966863f3c2feea9
rkg0612 <ralphkleinguevarra@gmail.com>
2022-10-03 10:37:47 +0800

    push for under construction

--- src/pages/404.js
+++ src/pages/404.js
@@ -37,6 +37,8 @@ export const NotFoundPage = () => {
           {/* <h1>NOT FOUND</h1>
           <p>You just hit a route that doesn&#39;t exist... the sadness.</p> */}
           {/* <Img fluid={data.notfound.childImageSharp.fluid} /> */}
+          <h1>UNDER CONSTRUCTION</h1>
+          <p>This site is still under construction. We apologize for the inconvinience!</p>
         </Style404>
       </Container>
     </Layout>
